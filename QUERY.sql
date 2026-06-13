0fba36e26f05832f28ab795f0336cd5b743312e1
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2026-06-14 00:20:12 +0600

    query 6 is done

--- QUERY.sql
+++ QUERY.sql
@@ -107,3 +107,10 @@ inner join matches m on b.match_id = m.match_id
 select u.user_id, u.full_name, b.booking_id
 from users u
 left join bookings b on b.user_id = u.user_id 
+
+
+-- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
+select booking_id, match_id, round(total_cost) total_cost
+from bookings
+where total_cost > (select avg(total_cost) from bookings) 
+
